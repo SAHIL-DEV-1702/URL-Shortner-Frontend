1d6986832d7c79e43af8dbd19e9d95f827bbe325
Sahil Patil <patilsahil@172002@gmail.com>
2026-07-12 12:59:18 +0530

    Update axios instance baseURL to production server

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000', // Change to your backend URL
+    baseURL: 'https://url-shortner-backend-1-7ems.onrender.com',
     timeout: 10000,
     withCredentials: true,
 
